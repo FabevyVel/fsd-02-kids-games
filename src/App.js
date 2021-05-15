a5e48b60cd6a180005f57cde39a7bd893dc13752
FabevyVel <vel@fabevy.com>
2021-05-15 18:12:13 +0530

    add table of games.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,79 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Game Name</th>
+            <th>Category</th>
+            <th>Done By</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Series</td>
+            <td>Numbers</td>
+            <td>Harish</td>
+          </tr>
+          <tr>
+            <td>Between</td>
+            <td>Numbers</td>
+            <td>Gowtham</td>
+          </tr>
+          <tr>
+            <td>Before &amp; After</td>
+            <td>Numbers</td>
+            <td>Ashwin</td>
+          </tr>
+          <tr>
+            <td>Addition &amp; Sub</td>
+            <td>Numbers</td>
+            <td>Mathan</td>
+          </tr>
+          <tr>
+            <td>Series</td>
+            <td>Alphapets</td>
+            <td>Ram</td>
+          </tr>
+          <tr>
+            <td>Between</td>
+            <td>Alphapets</td>
+            <td>Monisha</td>
+          </tr>
+          <tr>
+            <td>Before &amp; After</td>
+            <td>Alphapets</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Before &amp; After</td>
+            <td>Days</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Spelling Check</td>
+            <td>Days</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Between</td>
+            <td>Days</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Yesterday | Today | Tomorrow</td>
+            <td>Days</td>
+            <td>Vel</td>
+          </tr>
+          <tr>
+            <td>Form the cycle</td>
+            <td>Days</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
